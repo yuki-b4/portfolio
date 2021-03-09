90b50227fb5a5b3c7abc005f81b93682449f90ab
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-03-09 12:28:08 +0900

    成果物ページの作成と、ヘッダーのリンク化

---
+++ app/controllers/productions_controller.rb
@@ -0,0 +1,5 @@
+class ProductionsController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "status#index"
   resources :status, only: [:index]
   resources :contacts, only: [:new, :create]
+  resources :productions, only: :index
 end
